b936f41b5e9cbccdb98bfa43f9ca8ffe1e93b0ca
sanjana-d7 <158017404+sanjana-d7@users.noreply.github.com>
2024-04-16 02:41:00 +0000

    edits

--- src/main/java/uta/cse3310/Timer.java
+++ src/main/java/uta/cse3310/Timer.java
@@ -3,16 +3,38 @@ package uta.cse3310;
 public class Timer {
     private long startTime;
     private long durationInMillis;
+    private boolean isRunning;
+    private Runnable timeUpCallback; 
 
-    public void setTime(long time){
-
+    public void setTime(long timeInMillis) {
+        this.durationInMillis = timeInMillis;
     }
 
-    public void startTime(long start_time){
-
+    
+    public void startTime() {
+        this.startTime = System.currentTimeMillis();
+        this.isRunning = true;
     }
 
-    public boolean isTimeUp(){
-        return true;
+ 
+    public void setTimeUpCallback(Runnable callback) {
+        this.timeUpCallback = callback;
+    }
+
+  
+    public boolean isTimeUp() {
+        long currentTime = System.currentTimeMillis();
+        long elapsedTime = currentTime - startTime;
+
+        if (isRunning && elapsedTime >= durationInMillis) {
+            isRunning = false;
+
+          
+            if (timeUpCallback != null) {
+                timeUpCallback.run();
+            }
+            return true;
+        }
+        return false;
     }
 }
