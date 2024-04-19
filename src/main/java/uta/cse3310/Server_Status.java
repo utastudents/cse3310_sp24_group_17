b4fb587ae69ed55077af3a1d47b885737400ed08
sanjana-d7 <158017404+sanjana-d7@users.noreply.github.com>
2024-04-18 21:41:53 -0500

    Update Server_Status.java

--- src/main/java/uta/cse3310/Server_Status.java
+++ src/main/java/uta/cse3310/Server_Status.java
@@ -1,22 +1,39 @@
 package uta.cse3310;
 
 public class Server_Status {
-    private int Concurrent_Games;
-    private int total_Players;
+    private int concurrentGames;
+    private int totalPlayers;
+    private int maxConcurrentGames; 
+    private int maxTotalPlayers;
 
-    public void update_Status(MainLobby lobby){
-        
+    // Constructor
+    public Server_Status(int maxConcurrentGames, int maxTotalPlayers) {
+        this.maxConcurrentGames = maxConcurrentGames;
+        this.maxTotalPlayers = maxTotalPlayers;
+        this.concurrentGames = 0;
+        this.totalPlayers = 0;
     }
 
-    boolean isMaxGames(){
-        return true;
+    
+    public void updateStatus(MainLobby lobby) {
+       
+        this.concurrentGames = lobby.getConcurrentGames();
+        this.totalPlayers = lobby.getTotalPlayers();
     }
 
-    boolean isMaxPlayers(){
-        return true;
+   
+    public boolean isMaxGames() {
+        return concurrentGames >= maxConcurrentGames;
     }
 
-    void display_Status(){
-        
+    public boolean isMaxPlayers() {
+        return totalPlayers >= maxTotalPlayers;
+    }
+
+   
+    public void displayStatus() {
+        System.out.println("Server Status:");
+        System.out.println("Concurrent Games: " + concurrentGames + "/" + maxConcurrentGames);
+        System.out.println("Total Players: " + totalPlayers + "/" + maxTotalPlayers);
     }
 }
