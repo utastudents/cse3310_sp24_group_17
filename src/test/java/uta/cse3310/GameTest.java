6302ba55ebc1c0824f8043e12db9d3dfaf462ca7
Vmai711 <151262094+Vmai711@users.noreply.github.com>
2024-05-01 16:02:16 -0500

    Update GameTest.java

--- src/test/java/uta/cse3310/GameTest.java
+++ src/test/java/uta/cse3310/GameTest.java
@@ -1,37 +1,127 @@
 package uta.cse3310;
 import junit.framework.TestCase;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Random;
+import java.util.ArrayList;
 
-/*public class GameTest extends TestCase {
+public class GameTest extends TestCase {
 
-    public Game game;
-    private int max=20;
+    public Game Testgame;
+    // Test grid size
+    private int testsize = 20;                                     
+    // List of words      
+    String filePath = "src/test/java/uta/cse3310/testWords.txt";
+    // Number of words for testing
+    int numOfWords = 5;
     
+    // Test game set up
     public void setUp() { 
-        game = new Game("src/test/java/uta/cse3310/testWords.txt", 5, max,1); 
+        Testgame = new Game(filePath, numOfWords, testsize); 
+        assertNotNull(Testgame);
+        assertEquals(testsize, Testgame.getMatrix().length);
+        assertEquals(numOfWords, Testgame.getNumofWords());
+        assertNotNull(Testgame.getGameID());
+        assertNotNull(Testgame.getWordsInMatrix());
     }
     
-
-    public void testMatrixInitialization() {
-        game.initializwithdots();
-        char[][] matrix = game.getMatrix();
-        for (int i = 0; i < max; i++) {
-            for (int j = 0; j < max; j++) {
-                
-                assertEquals('.',matrix[i][j]);
-            }
-    }
-}
-    
+    // Test if the test words are placed in the grid
     public void testWordPlacement() {
-        
-        game.fillWithwords();
-        assertTrue("Expected at least one word to be placed", game.getNumofWords() > 0);
+        Testgame = new Game(filePath, numOfWords, testsize); 
+        char[][] matrix = Testgame.getMatrix();
+        ArrayList<String> wordsInMatrix = Testgame.getWordsInMatrix();
+    
+        for (String word : wordsInMatrix) {
+            boolean wordFound = false;
+            for (int i = 0; i < testsize; i++) {
+                for (int j = 0; j < testsize; j++) {
+                    if (matrix[i][j] == word.charAt(0)) {
+                        // Check horizontally
+                        if (j + word.length() <= testsize) {
+                            StringBuilder sb = new StringBuilder();
+                            for (int k = 0; k < word.length(); k++) {
+                                sb.append(matrix[i][j + k]);
+                            }
+                            if (sb.toString().equals(word)) {
+                                wordFound = true;
+                                assertTrue(word, wordFound);
+                                break;
+                            }
+                        }
+
+                        // Check vertically
+                        if (i + word.length() <= testsize) {
+                            StringBuilder sb = new StringBuilder();
+                            for (int k = 0; k < word.length(); k++) {
+                                sb.append(matrix[i + k][j]);
+                            }
+                            if (sb.toString().equals(word)) {
+                                wordFound = true;
+                                assertTrue(word, wordFound);
+                                break;
+                            }
+                        }
+
+                        // Check diagonally (top-left to bottom-right)
+                        if (i + word.length() <= testsize && j + word.length() <= testsize) {
+                            StringBuilder sb = new StringBuilder();
+                            for (int k = 0; k < word.length(); k++) {
+                                sb.append(matrix[i + k][j + k]);
+                            }
+                            if (sb.toString().equals(word)) {
+                                wordFound = true;
+                                assertTrue(word, wordFound);
+                                break;
+                            }
+                        }
+
+                        // Check diagonally (bottom-left to top-right)
+                        if (i - word.length() >= -1 && j + word.length() <= testsize) {
+                            StringBuilder sb = new StringBuilder();
+                            for (int k = 0; k < word.length(); k++) {
+                                sb.append(matrix[i - k][j + k]);
+                            }
+                            if (sb.toString().equals(word)) {
+                                wordFound = true;
+                                assertTrue(word, wordFound);
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (wordFound) {
+                    break;
+                }
+            }
+            // Some words may not be found due to testsize
+            System.out.println("Word '" + word + "' not found in the matrix.");
+        }
     }
 
+    // Test if a word can be found on a grid
+    public void testWordCheck() {
+        Testgame = new Game(filePath, numOfWords, testsize); 
+        ArrayList<WordDetail> wordsInMatrixDetails = Testgame.wordsInMatrixDetails;
 
+        for (WordDetail detail : wordsInMatrixDetails) {
+            int startRow = detail.startRow;
+            int startCol = detail.startCol;
+            int direction = detail.direction;
+            
+            // Adjust end coordinates based on word length and direction
+            int endRow = startRow;
+            int endCol = startCol;
+            int wordLength = detail.word.length();
+            switch (direction) {
+                case 0: endCol += wordLength - 1; break; // Right
+                case 1: endRow += wordLength - 1; break; // Down
+                case 2: endCol -= wordLength - 1; break; // Left
+                case 3: endRow -= wordLength - 1; break; // Up
+                case 4: endRow += wordLength - 1; endCol += wordLength - 1; break; // Diagonal Down-Right
+                case 5: endRow += wordLength - 1; endCol -= wordLength - 1; break; // Diagonal Down-Left
+                case 6: endRow -= wordLength - 1; endCol -= wordLength - 1; break; // Diagonal Up-Left
+                case 7: endRow -= wordLength - 1; endCol += wordLength - 1; break; // Diagonal Up-Right
+            }
+            
+            assertTrue("Word '" + detail.word + "' not found in the matrix.", Testgame.check_ans(startRow, startCol, endRow, endCol));
+        }
+    }
 }
-*/
